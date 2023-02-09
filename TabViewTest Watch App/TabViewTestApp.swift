10eba3eb4a3604efdb14779b577e44f8ccaf735c
Bryce Ellis <zbellis03@gmail.com>
2023-02-09 07:13:20 -0600

    Initial Commit

---
+++ TabViewTest Watch App/TabViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewTestApp.swift
+//  TabViewTest Watch App
+//
+//  Created by Bryce Ellis on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TabViewTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
