5075d8827abe0f807b08e7ec5360b4331e0bd88c
Bryce Ellis <zbellis03@gmail.com>
2023-02-26 16:26:32 -0600

    Active Calories

--- TabViewTest Watch App/ContentView.swift
+++ TabViewTest Watch App/ContentView.swift
@@ -1,46 +1,62 @@
-//
-//  ContentView.swift
-//  TabViewTest Watch App
-//
-//  Created by Bryce Ellis on 2/9/23.
-//
-
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
+    let healthStore = HKHealthStore()
+    @State private var activeEnergy: Double = 0.0
+    
     var body: some View {
-        NavigationView {
-            VStack {
-                    NavigationLink(destination: DetailView()) {
-                        Text("Go to Stress View")
-                }
-                NavigationLink(destination: DetailView2()) {
-                    Text("Go to Exercise View")
-            }
-                NavigationLink(destination: DetailView3()) {
-                    Text("Go to Rest View")
-            }
+        VStack {
+            Text("Active Energy")
+                .font(.headline)
+            Text("\(activeEnergy, specifier: "%.0f") kcal")
+                .font(.largeTitle)
+            Button {
+            authorizeHealthKit()
+            startActiveEnergyQuery()
                 
-            // Complete
+        } label: {
+            Image(systemName: "arrow.clockwise")
+        }
+            .cornerRadius(5)
+            .tint(.mint)
+        }
+        .onAppear {
+            authorizeHealthKit()
+            startActiveEnergyQuery()
+        }
+    }
+    
+    private func authorizeHealthKit() {
+        let activeEnergyType = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!
+        healthStore.requestAuthorization(toShare: nil, read: Set([activeEnergyType])) { (success, error) in
+            if success {
+                print("Successfully authorized to read active energy data")
+            } else {
+                print("Failed to authorize to read active energy data: \(error?.localizedDescription ?? "Unknown error")")
             }
         }
     }
-}
-
-struct DetailView: View {
-    var body: some View {
-        Text("This is the Stress view")
+    
+    private func startActiveEnergyQuery() {
+        let activeEnergyType = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictEndDate)
+        let query = HKStatisticsQuery(quantityType: activeEnergyType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
+            guard let result = result, let sum = result.sumQuantity() else {
+                print("Failed to retrieve active energy data: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            self.activeEnergy = sum.doubleValue(for: HKUnit.kilocalorie())
+        }
+        healthStore.execute(query)
     }
 }
 
-struct DetailView2: View {
-    var body: some View {
-        Text("This is the Exercise view")
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
-struct DetailView3: View {
-    var body: some View {
-        Text("This is the Rest view")
-    }
-}
