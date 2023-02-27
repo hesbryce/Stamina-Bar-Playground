e5f4ac0ad4d46c5e37124903143485c0b1afdfc4
Bryce Ellis <zbellis03@gmail.com>
2023-02-26 19:41:06 -0600

    Total calorie expendature

--- TabViewTest Watch App/ContentView.swift
+++ TabViewTest Watch App/ContentView.swift
@@ -4,6 +4,11 @@ import HealthKit
 struct ContentView: View {
     let healthStore = HKHealthStore()
     @State private var activeEnergy: Double = 0.0
+    @State private var restingEnergy: Double = 0.0
+    
+    var totalEnergy: Double {
+        return activeEnergy + restingEnergy
+    }
     
     var body: some View {
         VStack {
@@ -11,29 +16,32 @@ struct ContentView: View {
                 .font(.headline)
             Text("\(activeEnergy, specifier: "%.0f") kcal")
                 .font(.largeTitle)
-            Button {
-            authorizeHealthKit()
-            startActiveEnergyQuery()
-                
-        } label: {
-            Image(systemName: "arrow.clockwise")
-        }
-            .cornerRadius(5)
-            .tint(.mint)
+            
+            Text("Resting Energy")
+                .font(.headline)
+            Text("\(restingEnergy, specifier: "%.0f") kcal")
+                .font(.largeTitle)
+            
+            Text("Total Energy")
+                .font(.headline)
+            Text("\(totalEnergy, specifier: "%.0f") kcal")
+                .font(.largeTitle)
         }
         .onAppear {
             authorizeHealthKit()
             startActiveEnergyQuery()
+            startRestingEnergyQuery()
         }
     }
     
     private func authorizeHealthKit() {
         let activeEnergyType = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!
-        healthStore.requestAuthorization(toShare: nil, read: Set([activeEnergyType])) { (success, error) in
+        let restingEnergyType = HKObjectType.quantityType(forIdentifier: .basalEnergyBurned)!
+        healthStore.requestAuthorization(toShare: nil, read: Set([activeEnergyType, restingEnergyType])) { (success, error) in
             if success {
-                print("Successfully authorized to read active energy data")
+                print("Successfully authorized to read active and resting energy data")
             } else {
-                print("Failed to authorize to read active energy data: \(error?.localizedDescription ?? "Unknown error")")
+                print("Failed to authorize to read active and resting energy data: \(error?.localizedDescription ?? "Unknown error")")
             }
         }
     }
@@ -52,6 +60,21 @@ struct ContentView: View {
         }
         healthStore.execute(query)
     }
+    
+    private func startRestingEnergyQuery() {
+        let restingEnergyType = HKObjectType.quantityType(forIdentifier: .basalEnergyBurned)!
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictEndDate)
+        let query = HKStatisticsQuery(quantityType: restingEnergyType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
+            guard let result = result, let sum = result.sumQuantity() else {
+                print("Failed to retrieve resting energy data: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            self.restingEnergy = sum.doubleValue(for: HKUnit.kilocalorie())
+        }
+        healthStore.execute(query)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -59,4 +82,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
